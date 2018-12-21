891d445d9ba99e268720ee4fb05b6b076eb7345b
Paul Fell-Cinoman <swellpf@gmail.com>
2018-12-21 15:29:55 +0000

    first commit

---
+++ lib/bucs_roster_cli_gem.rb
@@ -0,0 +1,6 @@
+require "bucs_roster_cli_gem/version"
+
+module BucsRosterCliGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bucs_roster_cli_gem/version.rb
@@ -0,0 +1,3 @@
+module BucsRosterCliGem
+  VERSION = "0.1.0"
+end

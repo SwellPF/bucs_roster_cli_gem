57df0831e3a20f5dfdc5b3a017640d7ea811f38d
Paul Fell-Cinoman <swellpf@gmail.com>
2018-12-28 02:05:47 +0000

    fixed scrape_index method

--- lib/bucs_roster/player.rb
+++ lib/bucs_roster/player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_accessor :player_name, :position, :jersey_number, :height, :weight, :age, :experience, :college
+  attr_accessor :player_name, :position, :jersey_number, :height, :weight, :age, :experience, :college, :player_URL
   @@all = []
   
-  def initialize(player_name)
+  def initialize(player_name, player_URL)
     @player_name = player_name
+    @player_URL = player_URL
     @position = position
     @jersey_number = jersey_number
     @height = height
--- lib/bucs_roster/scraper.rb
+++ lib/bucs_roster/scraper.rb
@@ -3,12 +3,15 @@ class Scraper
   def scrape_index
     html = open("https://www.buccaneers.com/team/players-roster/")
     doc = Nokogiri::HTML(html)
-    doc.css(".nfl-o-roster__player-name a").each do |player|
-      player_name = player.css(".nfl-o-roster__player-name a").text
-      Player.new(player_name)
-    binding.pry
+    doc.css(".nfl-o-roster__player-name").each do |player|
+      player_name = player.css("a").text
+      player_URL = "https://www.buccaneers.com" + player.css("a")[0]["href"]
+      binding.pry
+      Player.new(player_name, player_URL)
+   # binding.pry
     end
     binding.pry
+    Player.all
   end
   
 end

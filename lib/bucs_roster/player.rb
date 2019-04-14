826b6e20b3d193e190841b7e5a657a046c0d4a3f
Paul <Paul@MacBook-Pro.local>
2019-04-14 15:58:23 -0400

    Spacing cleanup and integrating with GitHub for Atom

--- lib/bucs_roster/player.rb
+++ lib/bucs_roster/player.rb
@@ -1,24 +1,24 @@
 class Player
   attr_accessor :name, :position, :jersey_number, :height, :weight, :age, :experience, :college, :player_URL, :bio
   @@all = []
-  
+
   def initialize(name, player_URL)
     @name = name
     @player_URL = player_URL
-    @position = position
-    @jersey_number = jersey_number
-    @height = height
-    @weight = weight
-    @age = age
-    @experience = experience
-    @college = college
+#   @position = position
+#   @jersey_number = jersey_number
+#   @height = height
+#   @weight = weight
+#   @age = age
+#   @experience = experience
+#   @college = college
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.show_player_info(index)
     player = self.all[index]
     puts " ---- Tampa Bay Buccaneers Player Profile ----"
